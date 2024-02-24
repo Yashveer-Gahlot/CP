86f2426dde3272120a4ae2325d47a06a14835bf2
yash <109819396+Yashveer-Gahlot@users.noreply.github.com>
2024-02-24 09:06:53 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/* DATA_types and structures */
+#define long long ll
+#define double d
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define vs vector<string> 
+#define mii map<int, int>
+#define mci map<char,int>
+#define si set<int>
+#define sc set<char>
+
+
+/* Functions  */
+#define pb push_back
+#define eb emplace_back
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+
+
+/*------------------------------------------*/
+
+void solve(){
+    //your code
+    }
+    
+    
+
+}
+
+
+/*------------------------------------------*/
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+} 
