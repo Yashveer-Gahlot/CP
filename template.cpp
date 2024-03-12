4ec0ed0311fd728f350eac9f0891f2c0198ecb69
yash <109819396+Yashveer-Gahlot@users.noreply.github.com>
2024-03-12 17:05:15 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -2,8 +2,6 @@
 using namespace std;
 
 /* DATA_types and structures */
-#define long long ll
-#define double d
 #define pii pair<int, int>
 #define pll pair<long long, long long>
 #define vi vector<int>
@@ -13,6 +11,35 @@ using namespace std;
 #define mci map<char,int>
 #define si set<int>
 #define sc set<char>
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
 
 
 /* Functions  */
@@ -27,10 +54,7 @@ using namespace std;
 
 void solve(){
     //your code
-    }
     
-    
-
 }
 
 
